1cc3a30c3f9d55496cc609a909ec405d224e1585
Dimon Eir <deiramjani@gmail.com>
2021-07-10 06:39:16 -0700

    OAuth and user authentification

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '187370910018069',
+        clientSecret: 'c900e5134fe35de049e3016d76dfd93b'
+    }
 }
